f5f08843299b0544e799f3859ff0ffe62ecc602d
Zach Buttram <zach.buttram@gmail.com>
2016-09-23 16:23:27 -0700

    second test pub

--- presentation/index.js
+++ presentation/index.js
@@ -65,7 +65,7 @@ export default class Presentation extends React.Component {
             <Heading size={1} fit caps lineHeight={1}>
               Holy Script!
             </Heading>
-            <Heading size={1} fit caps textColor="secondary">
+            <Heading size={4} fit caps textColor="secondary">
               Scripting for the MSP
             </Heading>
             <Layout>
@@ -75,28 +75,31 @@ export default class Presentation extends React.Component {
           </Slide>
           <Slide>
             <Heading caps>Overview</Heading>
+            <Heading size={4} caps textColor="secondary">(What We'll Cover)</Heading>
             <List>
-              <Appear><ListItem>How can MSPs benefit from scripting knowledge?</ListItem></Appear>
-              <Appear><ListItem>Creating</ListItem></Appear>
-              <Appear><ListItem>Managing</ListItem></Appear>
-              <Appear><ListItem>Documenting</ListItem></Appear>
+              <Appear><ListItem>WHY: How can MSPs benefit from scripting knowledge?</ListItem></Appear>
+              <Appear><ListItem>WHAT: Understanding what options are available.</ListItem></Appear>
+              <Appear><ListItem>WHERE: Managing and deploying/publishing your scripts.</ListItem></Appear>
+              <Appear><ListItem>HOW: Documenting your scripts properly and efficiently.</ListItem></Appear>
               <Appear><ListItem>Resources & Continued Learning</ListItem></Appear>
             </List>
           </Slide>
           <Slide>
             <Heading caps>Why Scripts?</Heading>
             <List>
-              <Appear><ListItem>Quick Fixes</ListItem></Appear>
-              <Appear><ListItem>Monitoring</ListItem></Appear>
-              <Appear><ListItem>Macros</ListItem></Appear>
-              <Appear><ListItem>Data Gathering/Merging/Transformations</ListItem></Appear>
-              <Appear><ListItem>Computer Science</ListItem></Appear>
+              <Appear><ListItem>Implement Quick Fixes</ListItem></Appear>
+              <Appear><ListItem>Customize Your Monitoring</ListItem></Appear>
+              <Appear><ListItem>Create Macros for Repeated Tasks</ListItem></Appear>
+              <Appear><ListItem>Make Data Gathering/Merging/Transformations Easy</ListItem></Appear>
+              <Appear><ListItem>Apply Basic Computer Science to Everyday Issues</ListItem></Appear>
               <Appear><ListItem>Do More with Less</ListItem></Appear>
             </List>
+            <Appear><Text>Scripts can be useful for more than just support, service, and RMM! One or more of the applications and services you use day-to-day may be scriptable.</Text></Appear>
           </Slide>
           <Slide bgColor="black">
-            <Image src={images.toohard.replace("/", "")}/>
-            <Appear><Heading fit textColor="secondary">It doesn't have to be.</Heading></Appear>
+            <Heading fit textColor="secondary" lineHeight={1.5}>You might be thinking...</Heading>
+            <Appear><Image src={images.toohard.replace("/", "")}/></Appear>
+            <Appear><Heading fit textColor="primary">It doesn't have to be.</Heading></Appear>
           </Slide>
           <Slide notes="LabTech and Kaseya both have the ability to run PowerShell scripts. Kaseya was a bit more difficult to find documentation for, could just be me not having access to it.">
             <Heading fit caps lineHeight={1.5}>Simple, but Powerful</Heading>
@@ -117,14 +120,18 @@ export default class Presentation extends React.Component {
             </Appear>
           </Slide>
           <Slide notes="There's a level above Python we won't discuss. You can download and run scripts and executables with LabTech. ">
-            <Heading fit caps>Code Complexity</Heading>
+            <Heading fit caps>Languages and Services</Heading>
+            <Heading fit caps>In (Roughly) Order of Complexity</Heading>
             <List>
-              <Appear><ListItem>Python (with PyInstaller)</ListItem></Appear>
+              <Appear><ListItem>Python (with <Link href="http://www.pyinstaller.org/" target="_blank">PyInstaller</Link>)</ListItem></Appear>
               <Appear><ListItem>PowerShell</ListItem></Appear>
-              <Appear><ListItem>AutoHotKey / AutoIT</ListItem></Appear>
+              <Appear><ListItem><Link href="http://ahkscript.org/" target="_blank">AutoHotKey</Link> / <Link href="https://www.autoitscript.com/site/autoit/" target="_blank">AutoIT</Link></ListItem></Appear>
               <Appear><ListItem>Batch</ListItem></Appear>
-              <Appear><ListItem>IFTTT / Zapier</ListItem></Appear>
+              <Appear><ListItem><Link href="https://ifttt.com/" target="_blank">IFTTT</Link> / <Link href="https://zapier.com/" target="_blank">Zapier</Link></ListItem></Appear>
             </List>
+          </Slide>
+          <Slide>
+            <Heading fit caps>Git: It's Not Just for Developers</Heading>
           </Slide>
         </Deck>
       </Spectacle>
